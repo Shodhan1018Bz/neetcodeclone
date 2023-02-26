2b1c43c1ef394dfd437d05e8a8f657bbd7deb19d
Shodhan1018Bz <121626436+Shodhan1018Bz@users.noreply.github.com>
2023-02-26 15:12:47 +0530

    two functions are added for create&update course

--- backend/lib/courselib.js
+++ backend/lib/courselib.js
@@ -3,6 +3,15 @@ module.exports.getAllCourses=async function(){
         return await courseModel.find({});
 }
 
+module.exports.createCourse=async function(courseInput){
+    const course= new courseModel(courseInput);
+    await course.save();
+}
+
+module.exports.updateCourse=async function(courseId,updateInput){
+    await courseModel.findOneAndUpdate({_id:courseId},updateInput);
+}
+
 module.exports.createFirstCourse=async function(){
     const courses=await courseModel.find({});
     if(courses && courses.length>=1)
