e80ca6609e28473ce66a84ffadc04e15c8ee4f96
Shodhan1018Bz <121626436+Shodhan1018Bz@users.noreply.github.com>
2023-02-26 14:14:52 +0530

    Adding mongodb to project

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 const express=require("express");
-
+const dotenv=require("dotenv");
+const { default: mongoose } = require("mongoose");
+dotenv.config();
 const app= express();
-
+const str=process.env.MONGO_CONNECTION_STRING;
 app.use(express.static('frontend'));
 
 app.get("/",(req,res)=>{
     res.sendFile(__dirname+"/frontend/index.html");
 })
-
-
-
-app.listen(3000,function(){
-    console.log("Server is running.... on port 3000")
+mongoose.set('strictQuery', true);
+mongoose.connect(str,(err)=>{
+    if(err){
+        console.log(err)
+    }
+    else{
+        app.listen(3000,function(){
+            console.log("Connected to database");
+            console.log("Server is running.... on port 3000")
+        })
+    }
+    
 })
+
